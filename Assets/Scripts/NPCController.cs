1e7d29b79aedd5ba9114baa35a02bb52371a2106
IAN! <Iannorton01@gmail.com>
2018-05-05 22:53:20 -0400

    Imported Nuha's Dogs to branch. Setup NPC pathing.

--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -16,10 +16,10 @@ public class NPCController : MonoBehaviour {
     bool resting = false;
 
     [Header("Dialogue")]
-    [SerializeField] Canvas dialogueCanvas;
-    [SerializeField] GameObject NPCEmote;
-    [SerializeField] string[] fartReactionArray;
-    [SerializeField] string[] playerCalloutArray;
+   // [SerializeField] Canvas dialogueCanvas;
+   // [SerializeField] GameObject NPCEmote;
+   // [SerializeField] string[] fartReactionArray;
+   // [SerializeField] string[] playerCalloutArray;
 
     Rigidbody2D rb2d;
 
@@ -27,7 +27,7 @@ public class NPCController : MonoBehaviour {
 	void Start () {
 
         rb2d = this.transform.GetComponentInChildren<Rigidbody2D>();
-        dialogueCanvas.gameObject.SetActive(false);
+        //dialogueCanvas.gameObject.SetActive(false);
 
         if (waypoints.Length == 0) { resting = true; }
         else { this.transform.position = waypoints[waypointIndex].transform.position; }
@@ -45,10 +45,10 @@ public class NPCController : MonoBehaviour {
             // Flip sprite if necessary
             if((waypoints[waypointIndex].transform.position.x > this.transform.position.x && this.transform.localScale.x > 0) ||
                 (waypoints[waypointIndex].transform.position.x < this.transform.position.x && this.transform.localScale.x < 0))
-            {
-                this.transform.localScale = new Vector3(-this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z);
-                dialogueCanvas.GetComponentInChildren<Text>().transform.localScale = new Vector3(-dialogueCanvas.GetComponentInChildren<Text>().transform.localScale.x, dialogueCanvas.GetComponentInChildren<Text>().transform.localScale.y, dialogueCanvas.GetComponentInChildren<Text>().transform.localScale.z);
-            }
+           {
+              this.transform.localScale = new Vector3(-this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z);
+          //     dialogueCanvas.GetComponentInChildren<Text>().transform.localScale = new Vector3(-dialogueCanvas.GetComponentInChildren<Text>().transform.localScale.x, dialogueCanvas.GetComponentInChildren<Text>().transform.localScale.y, dialogueCanvas.GetComponentInChildren<Text>().transform.localScale.z);
+           }
         
             if(distance > distanceRequired)
             {
@@ -62,17 +62,17 @@ public class NPCController : MonoBehaviour {
         }
 	}
 
-    void GetAngry()
-    {
-        NPCEmote.GetComponent<Animator>().SetBool("IsAngry", true);
-        StartCoroutine(CalmDown());
-    }
-
-    IEnumerator CalmDown()
-    {
-        yield return new WaitForSeconds(5);
-        NPCEmote.GetComponent<Animator>().SetBool("IsAngry", false);
-    }
+   //void GetAngry()
+   //{
+   //    NPCEmote.GetComponent<Animator>().SetBool("IsAngry", true);
+   //    StartCoroutine(CalmDown());
+   //}
+   //
+   //IEnumerator CalmDown()
+   //{
+   //    yield return new WaitForSeconds(5);
+   //    NPCEmote.GetComponent<Animator>().SetBool("IsAngry", false);
+   //}
 
     IEnumerator WaitAtWaypoint ()
     {
@@ -86,24 +86,24 @@ public class NPCController : MonoBehaviour {
         if (waypointIndex >= waypoints.Length) { waypointIndex = 0; }
     }
 
-    public void SmellFart ()
-    {
-        GetAngry();
-        dialogueCanvas.gameObject.SetActive(true);
-        dialogueCanvas.GetComponentInChildren<Text>().text = fartReactionArray[Random.Range(0, fartReactionArray.Length)];
-        StartCoroutine(HideDialogueBox());
-    }
-
-    public void SayHi()
-    {
-        dialogueCanvas.gameObject.SetActive(true);
-        dialogueCanvas.GetComponentInChildren<Text>().text = playerCalloutArray[Random.Range(0, playerCalloutArray.Length)];
-        StartCoroutine(HideDialogueBox());
-    }
-
-    IEnumerator HideDialogueBox ()
-    {
-        yield return new WaitForSeconds(3);
-        dialogueCanvas.gameObject.SetActive(false);
-    }
+  //  public void SmellFart ()
+  //  {
+  //      GetAngry();
+  //      dialogueCanvas.gameObject.SetActive(true);
+  //      dialogueCanvas.GetComponentInChildren<Text>().text = fartReactionArray[Random.Range(0, fartReactionArray.Length)];
+  //      StartCoroutine(HideDialogueBox());
+  //  }
+  //
+  //  public void SayHi()
+  //  {
+  //      dialogueCanvas.gameObject.SetActive(true);
+  //      dialogueCanvas.GetComponentInChildren<Text>().text = playerCalloutArray[Random.Range(0, playerCalloutArray.Length)];
+  //      StartCoroutine(HideDialogueBox());
+  //  }
+  //
+  //  IEnumerator HideDialogueBox ()
+  //  {
+  //      yield return new WaitForSeconds(3);
+  //      dialogueCanvas.gameObject.SetActive(false);
+  //  }
 }
